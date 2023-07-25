73a946d5714dddaf8ab3c39510437234b242b006
Luis Eduardo <luis_cg.github@hotmail.com>
2023-07-25 14:59:13 -0300

    Order, Instant, ISO 8601 complete

---
+++ src/main/java/com/luisdev/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.luisdev.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luisdev.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
---
+++ src/main/java/com/luisdev/course/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.luisdev.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luisdev.course.entities.Order;
+import com.luisdev.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
