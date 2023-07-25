2ec7fe3ac015170c309905e6fa848dc5b66b80e1
Luis Eduardo <luis_cg.github@hotmail.com>
2023-07-25 14:30:58 -0300

    Order, Instant, ISO 8601, part one

--- src/main/java/com/luisdev/course/config/TestConfig.java
+++ src/main/java/com/luisdev/course/config/TestConfig.java
@@ -23,8 +23,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User u3 = new User(null, "test", "test@gmail.com", "123456789", "1234569812");
 		
-		userRepository.saveAll(Arrays.asList(u1,u2));
+		userRepository.saveAll(Arrays.asList(u1,u2,u3));
 	}
 
 }
